c8e992cd336bfc7b3359839e2f0fcb220fc5a250
Shadow Smith <hey@shadowsmith.com>
2023-04-25 11:47:38 -0500

    Add api error handling

--- api/index.js
+++ api/index.js
@@ -3,10 +3,15 @@ const packages = require("all-the-package-names")
 
 export default async (_request, res) => {
   const npmPackage = packages[Math.floor(Math.random() * packages.length)]
-  const response = await axios.get(`https://registry.npmjs.org/${npmPackage}`)
+  
+  try {
+    const response = await axios.get(`https://registry.npmjs.org/${npmPackage}`)
 
-  res.send({
-    npmURL: `https://www.npmjs.com/package/${npmPackage}`,
-    ...response.data
-  })
+    res.send({
+      npmURL: `https://www.npmjs.com/package/${npmPackage}`,
+      ...response.data
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
 }
