37c597d9b5864550789195e8d0871191fb74f82d
Shadow Smith <hey@shadowsmith.com>
2023-04-24 22:35:38 -0500

    Add Cache-Control header

--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ const packages = require("all-the-package-names")
 const npmPackage = packages[Math.floor(Math.random() * packages.length)]
  
 export default (_request, response) => {
+  response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
   response.redirect(`https://www.npmjs.com/package/${npmPackage}`, 200)
 }
