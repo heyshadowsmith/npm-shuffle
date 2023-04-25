a385df9ce9ef062673054a2e843e8d29a01e40d6
Shadow Smith <hey@shadowsmith.com>
2023-04-25 11:43:42 -0500

    Add npm Shuffle client

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
+const axios = require('axios')
 const packages = require("all-the-package-names")
 
-export default (_request, response) => {
+export default async (_request, res) => {
   const npmPackage = packages[Math.floor(Math.random() * packages.length)]
-  response.redirect(`https://www.npmjs.com/package/${npmPackage}`, 200)
+  const response = await axios.get(`https://registry.npmjs.org/${npmPackage}`)
+
+  res.send({
+    npmURL: `https://www.npmjs.com/package/${npmPackage}`,
+    ...response.data
+  })
 }
