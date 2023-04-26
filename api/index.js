cde19a98d5246c012da4320b6d93c6855c7f49d0
Shadow Smith <hey@shadowsmith.com>
2023-04-25 21:30:01 -0500

    Fix api

--- api/index.js
+++ api/index.js
@@ -2,17 +2,7 @@ const fetch = require('node-fetch')
 const packages = require('all-the-package-names')
 
 export default async (req, res) => {
-  // Vercel times out before this finishes //
-  // let monthlyDownloads = req.query?.downloads ?? 0
-  // let downloads = 0
-
-  // let npmPackage = pickNpmPackage()
-  // downloads = await getDownloads(npmPackage)
-
-  // do {
-  //   npmPackage = pickNpmPackage()
-  //   downloads = await getDownloads(npmPackage)
-  // } while (downloads <= monthlyDownloads)
+  let npmPackage = packages[Math.floor(Math.random() * packages.length)]
   
   try {
     const response = await fetch(`https://registry.npmjs.org/${npmPackage}`)
@@ -22,20 +12,6 @@ export default async (req, res) => {
       npmURL: `https://www.npmjs.com/package/${npmPackage}`,
       ...data
     })
-  } catch (error) {
-    throw new Error(error)
-  }
-}
-
-function pickNpmPackage () {
-  return packages[Math.floor(Math.random() * packages.length)]
-}
-
-async function getDownloads (npmPackage) {
-  try {
-    const response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${npmPackage}`)
-    const data = await response.json()
-    return data.downloads
   } catch (error) {
     throw new Error(error)
   }
