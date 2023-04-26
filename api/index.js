6d3d62e41d5969be796144a766c347d4b9656607
Shadow Smith <hey@shadowsmith.com>
2023-04-25 21:22:21 -0500

    Add downloads floor

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const fetch = require('node-fetch')
 const packages = require('all-the-package-names')
 
-export default async (_request, res) => {
-  const npmPackage = packages[Math.floor(Math.random() * packages.length)]
+export default async (req, res) => {
+  let monthlyDownloads = req.query?.downloads ?? 0
+  let downloads = 0
+
+  let npmPackage = pickNpmPackage()
+  downloads = await getDownloads(npmPackage)
+
+  do {
+    npmPackage = pickNpmPackage()
+    downloads = await getDownloads(npmPackage)
+  } while (downloads <= monthlyDownloads)
+
+  console.log('Downloads:', downloads)
+  console.log('Monthly Downloads:', monthlyDownloads)
   
   try {
     const response = await fetch(`https://registry.npmjs.org/${npmPackage}`)
@@ -12,6 +24,20 @@ export default async (_request, res) => {
       npmURL: `https://www.npmjs.com/package/${npmPackage}`,
       ...data
     })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+function pickNpmPackage () {
+  return packages[Math.floor(Math.random() * packages.length)]
+}
+
+async function getDownloads (npmPackage) {
+  try {
+    const response = await fetch(`https://api.npmjs.org/downloads/point/last-month/${npmPackage}`)
+    const data = await response.json()
+    return data.downloads
   } catch (error) {
     throw new Error(error)
   }
